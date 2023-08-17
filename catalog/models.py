7c2ecda455f6c131c9628084e8fa24187a28b380
Nodar <nodar.panchenko@gmail.com>
2023-08-17 18:14:05 +0400

    Написал фикстуры.

--- catalog/models.py
+++ catalog/models.py
@@ -7,12 +7,12 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
-    overview = models.TextField(max_length=255, verbose_name='Описание')
+    overview = models.TextField(max_length=255, verbose_name='Описание', **NULLABLE)
     preview = models.ImageField(upload_to='media/', verbose_name='Изображение', **NULLABLE)
     category = models.CharField(max_length=255, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за покупку')
     date_of_creation = models.DateTimeField(verbose_name='Дата создания')
-    last_update = models.DateTimeField(verbose_name='Дата последнего изменения')
+    last_update = models.DateTimeField(verbose_name='Дата последнего изменения', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} {self.overview} {self.preview} {self.category} {self.price} {self.date_of_creation} ' \
@@ -25,7 +25,7 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
-    overview = models.TextField(max_length=255, verbose_name='Описание')
+    overview = models.TextField(max_length=255, verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} {self.overview}'
