85c9c77fdc11d425b3111bc39507f971c429a09b
Nodar <nodar.panchenko@gmail.com>
2023-08-20 18:11:58 +0400

    Доработки для связывания 2 таблиц.

--- catalog/management/commands/update_db.py
+++ catalog/management/commands/update_db.py
@@ -1,7 +1,5 @@
-import json
-
 from django.core.management import BaseCommand
-
+from django.db import connection
 from catalog.models import Category, Product
 
 
@@ -11,17 +9,57 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-        category_list = json.load(open('fixture_for_category.json', 'r'))
-        product_list = json.load(open('fixture_for_products.json', 'r'))
+        # Сбрасываю id в БД
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT setval('catalog_category_id_seq', 1, false);"
+                           "SELECT setval('catalog_product_id_seq', 1, false)")
 
+        product_list = [
+            {"name": "Orange",
+             "overview": "",
+             "preview": "",
+             "category": "Fruits",
+             "price": 25,
+             "date_of_creation": "2023-08-17T13:47:17Z",
+             "last_update": None},
+            {"name": "Apple",
+             "overview": "",
+             "preview": "",
+             "category": "Fruits",
+             "price": 20,
+             "date_of_creation": "2023-08-17T13:48:20Z",
+             "last_update": None},
+            {"name": "Banana",
+             "overview": "",
+             "preview": "",
+             "category": "Fruits",
+             "price": 10,
+             "date_of_creation": "2023-08-17T13:48:43Z",
+             "last_update": None},
+            {"name": "Iphone 13",
+             "overview": "", "preview": "",
+             "category": "Mobile",
+             "price": 10,
+             "date_of_creation": "2023-08-17T13:49:10Z",
+             "last_update": None}
+        ]
+
+        category_set = set()
         category_for_create = []
         products_for_create = []
 
-        for category in category_list:
-            category_for_create.append(Category(**category['fields']))
+        for category in product_list:
+            category_set.add(category['category'])
 
-        for product in product_list:
-            products_for_create.append(Product(**product['fields']))
+        for category in category_set:
+            category = {'name': category}
+            category_for_create.append(Category(**category))
 
         Category.objects.bulk_create(category_for_create)
+
+        for product in product_list:
+            category = Category.objects.get(name=product['category'])
+            product['category'] = category
+            products_for_create.append(Product(**product))
+
         Product.objects.bulk_create(products_for_create)
--- catalog/models.py
+++ catalog/models.py
@@ -5,11 +5,23 @@ from django.db import models
 NULLABLE = {'null': True, 'blank': True}
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Наименование')
+    overview = models.TextField(max_length=255, verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} {self.overview}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
     overview = models.TextField(max_length=255, verbose_name='Описание', **NULLABLE)
     preview = models.ImageField(upload_to='media/', verbose_name='Изображение', **NULLABLE)
-    category = models.CharField(max_length=255, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name='Цена за покупку')
     date_of_creation = models.DateTimeField(verbose_name='Дата создания')
     last_update = models.DateTimeField(verbose_name='Дата последнего изменения', **NULLABLE)
@@ -21,15 +33,3 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Наименование')
-    overview = models.TextField(max_length=255, verbose_name='Описание', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.name} {self.overview}'
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
