5ebedcd579393c406e87298325a332a82d95a804
Nodar <nodar.panchenko@gmail.com>
2023-08-23 20:17:56 +0400

    Выполнил hw, все 4 задания. Добавил котроллер для новой страницы, устранил повторяющееся части шаблонов. Добавил изображения.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import home_page, contact_page
+from catalog.views import home_page, contact_page, products_page
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('contact/', contact_page, name='contact')
+    path('contact/', contact_page, name='contact'),
+    path('products/', products_page, name='products'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home_page(request):
@@ -13,3 +15,11 @@ def contact_page(request):
         message = request.POST.get('message')
         print(f'{name}, {phone}, {message}')
     return render(request, 'main/contact_page.html')
+
+
+def products_page(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'main/products_page.html', context)
